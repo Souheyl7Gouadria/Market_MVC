4a0d9f83cf7f888594019ee3aa6554111581d5c5
souheyl gouadria <souheyl.gouadria@supcom.tn>
2025-11-01 20:41:24 +0100

    Repository interface implementation

---
+++ Market.DataAccess/Repository/Repository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Market.DataAccess.Data;
+using Market.DataAccess.Repository.IRepository;
+using Market.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Market.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+        private readonly ApplicationDbContext _dbContext;
+        internal DbSet<T> _dbSet;
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();// here _dbSet = _dbContext.Categories;
+        }
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = _dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = _dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            _dbSet.RemoveRange(entity);
+        }
+    }
+}
