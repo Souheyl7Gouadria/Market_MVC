935f9918f6f3d6f359785f7292c3236e177c120e
souheyl gouadria <souheyl.gouadria@supcom.tn>
2025-11-09 16:31:23 +0100

    implemented ViewModel, upload image functionality and included navigation property in query

--- MarketWeb/Areas/Admin/Controllers/ProductController.cs
+++ MarketWeb/Areas/Admin/Controllers/ProductController.cs
@@ -2,8 +2,11 @@
 using Market.DataAccess.Repository;
 using Market.DataAccess.Repository.IRepository;
 using Market.Models;
+using Market.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace MarketWeb.Areas.Admin.Controllers
 {
@@ -11,61 +14,92 @@ namespace MarketWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _hostEnvironment;// used to access wwwroot folder
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
         }
         public IActionResult Index()
         {
-            List<Product> Products = _unitOfWork.ProductRepository.GetAll().ToList();
+            List<Product> Products = _unitOfWork.ProductRepository.GetAll(includeProperties: "Category").ToList();
             return View(Products);
         }
 
-        public IActionResult Create()
+        public IActionResult CreateOrUpdate(int? id)
         {
-            return View();
+            ProductVM productVM = new()
+            {
+                CategoryList = GetCategoryList(),
+                Product = new Product()
+            };
+
+            if (id == null || id == 0)
+            {
+                // create
+                return View(productVM);
+            }
+            else
+            {
+                // update
+                productVM.Product = _unitOfWork.ProductRepository.Get(u => u.Id == id);
+                if (productVM.Product == null)
+                {
+                    return NotFound();
+                }
+                return View(productVM);
+            }
         }
 
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult CreateOrUpdate(ProductVM productVM, IFormFile? file)
         {
+            if (string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                {
+                    productVM.Product.ImageUrl = "";
+                }
+
             if (ModelState.IsValid)
             {
-                _unitOfWork.ProductRepository.Add(obj);
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        // delete old image
+                        var oldImagePath = Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath)){
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
+                if (productVM.Product.Id == 0)
+                {
+                    _unitOfWork.ProductRepository.Add(productVM.Product);
+                    TempData["success"] = "Product created successfully";
+                }
+                else
+                {
+                    _unitOfWork.ProductRepository.Update(productVM.Product);
+                    TempData["success"] = "Product updated successfully";
+                }
+
                 _unitOfWork.Save();
-                TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
             }
-            return View(obj);
-        }
+  
+            productVM.CategoryList = GetCategoryList();
 
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if(id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product ProductFromDb = _unitOfWork.ProductRepository.Get(u => u.Id == id);
-            if (ProductFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(ProductFromDb);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            // Note: if the primary key name is not "Id", model binding will fail to bind the form data to the obj parameter, and will set the Id to 0
-            // and will create a new object instead of updating the existing one. to avoid that we need to add a hidden input field in the form with the name of the primary key.
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.ProductRepository.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product updated successfully";
-                return RedirectToAction("Index");
-            }
-            return View();
+            return View(productVM);
         }
 
         public IActionResult Delete(int? id)
@@ -92,8 +126,18 @@ namespace MarketWeb.Areas.Admin.Controllers
             }
             _unitOfWork.ProductRepository.Remove(ProductFromDb);
             _unitOfWork.Save();
-            TempData["success"] = "Product deleted  successfully";
+            TempData["success"] = "Product deleted successfully";
             return RedirectToAction("Index");
+        }
+        
+        private IEnumerable<SelectListItem> GetCategoryList()
+        {
+            return _unitOfWork.CategoryRepository.GetAll()
+                .Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.CategoryId.ToString()
+                });
         }
     }
 }
