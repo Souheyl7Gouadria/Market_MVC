dd1dcb2ff87791fadd401eb790e49f151d523753
souheyl gouadria <souheyl.gouadria@supcom.tn>
2025-11-15 22:11:39 +0100

    make use of ASP.NET core Identity

--- MarketWeb/Program.cs
+++ MarketWeb/Program.cs
@@ -2,6 +2,7 @@ using Market.DataAccess.Data;
 using Market.DataAccess.Repository;
 using Market.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+//1- register identity service in DI container (use built in IdentityUser class)
+//2- configure identity to use EF Core with ApplicationDbContext to create identity tables
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
@@ -26,9 +31,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
