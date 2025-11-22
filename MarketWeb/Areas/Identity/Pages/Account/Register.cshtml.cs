2dba86f065cb6522445a17050e45775b4b1ddb2c
souheyl gouadria <souheyl.gouadria@supcom.tn>
2025-11-22 14:27:56 +0100

    populate company field in user table

--- MarketWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MarketWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -146,6 +146,11 @@ namespace MarketWeb.Areas.Identity.Pages.Account
                 user.PostalCode = Input.PostalCode;
                 user.PhoneNumber = Input.PhoneNumber;
 
+                if (Input.Role == StaticDetails.Role_Company)
+                {
+                    user.CompanyId = Input.CompanyId;
+                }
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
